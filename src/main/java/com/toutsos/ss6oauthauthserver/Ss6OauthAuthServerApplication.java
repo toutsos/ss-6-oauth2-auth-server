beeffc7331aa838c1f4fbcc371da9a29a456aeef
toutsos <a.a.toutsios@army.gr>
2023-03-17 13:19:28 +0200

    first commit

---
+++ src/main/java/com/toutsos/ss6oauthauthserver/Ss6OauthAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.toutsos.ss6oauthauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss6OauthAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss6OauthAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toutsos/ss6oauthauthserver/Ss6OauthAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toutsos.ss6oauthauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss6OauthAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
